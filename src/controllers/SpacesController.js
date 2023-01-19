d156b46d14d29c613b7cf3e81982bbf6c39a2095
Bruno Siqueira <bruno@Brunos-MacBook-Air.local>
2023-01-19 16:49:36 -0300

    pg version and port update

--- src/controllers/SpacesController.js
+++ src/controllers/SpacesController.js
@@ -3,7 +3,11 @@ const knex = require('../database')
 module.exports = {
   async index(req, res) {
     try {
-      const results = await knex('orgs').withSchema('product_db')
+      const results = await knex('spaces')
+        .select('id')
+        .withSchema('product_db')
+        .limit(1)
+
       res.json(results)
     } catch(e) {
       console.error(e)
